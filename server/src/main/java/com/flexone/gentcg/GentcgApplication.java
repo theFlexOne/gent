4887d94fba667a7eff3655e11fb2cf84fde4ead7
flexone <edhavey@gmail.com>
2025-05-27 23:27:14 -0500

    .gitignore is now working

---
+++ server/src/main/java/com/flexone/gentcg/GentcgApplication.java
@@ -0,0 +1,13 @@
+package com.flexone.gentcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GentcgApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/flexone/gentcg/GentcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flexone.gentcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GentcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
